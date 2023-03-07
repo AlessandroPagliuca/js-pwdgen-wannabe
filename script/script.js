7221191b147f4930ec1ce23d618dba496ac38d35
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-07 16:45:42 +0100

    fix let yourPassword

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ const num = 21;
 let firstNameUser = prompt("Please enter your first name");
 let lastNameUser = prompt("Please enter your last name");
 let favoriteColor = prompt("Please enter your favorite color");
-let yourPassword = `Your password is : ${firstNameUser} ${lastNameUser} ${favoriteColor} ${num}`;
+let yourPassword = (firstNameUser + lastNameUser + favoriteColor + num);
 document.getElementById("password").innerHTML = yourPassword;
 
 console.log(`${yourPassword}`);
