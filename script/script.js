11b9b5d4aaf6dd0e47af76cdbe9f594f239ebac9
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-07 15:40:37 +0100

    fix prompt

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@
 const num = 21;
 
 //input client prompt
-let firstNameUser = prompt ("Please enter your first name");
-let lastNameUser = prompt ("Please enter your last name");
+let firstNameUser = prompt("Please enter your first name");
+let lastNameUser = prompt("Please enter your last name");
 let favoriteColor = prompt("Please enter your favorite color");
 let yourPassword = `Your password is : ${firstNameUser} ${lastNameUser} ${favoriteColor} ${num}`;
 document.getElementById("password").innerHTML = yourPassword;
